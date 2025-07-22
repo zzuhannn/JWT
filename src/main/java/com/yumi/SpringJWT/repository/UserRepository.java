decb07057dfb94bc476a5f4effe66219a59b88af
zzuhannn <lunaaeric41@naver.com>
2025-07-22 16:01:22 +0900

    feat: add method for find username in DB

--- src/main/java/com/yumi/SpringJWT/repository/UserRepository.java
+++ src/main/java/com/yumi/SpringJWT/repository/UserRepository.java
@@ -9,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Boolean existsByUsername(String username);
+
+    UserEntity findByUsername(String username);
 }
