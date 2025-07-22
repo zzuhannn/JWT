7b2005f4f5a85a7385bd3fbe95ad57e02719c816
zzuhannn <lunaaeric41@naver.com>
2025-07-22 14:33:43 +0900

    feat: add JoinDTO for user registration

---
+++ src/main/java/com/yumi/SpringJWT/dto/JoinDTO.java
@@ -0,0 +1,12 @@
+package com.yumi.SpringJWT.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinDTO {
+
+    private String username;
+    private String password;
+}
