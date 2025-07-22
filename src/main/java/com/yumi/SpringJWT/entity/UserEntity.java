07b512f334e22d7dca890bcdaa669b4e8b59075c
zzuhannn <lunaaeric41@naver.com>
2025-07-22 14:53:30 +0900

    feat: add Setter anotaion

--- src/main/java/com/yumi/SpringJWT/entity/UserEntity.java
+++ src/main/java/com/yumi/SpringJWT/entity/UserEntity.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Setter;
 
 @Entity
+@Setter
 public class UserEntity {
 
     @Id
