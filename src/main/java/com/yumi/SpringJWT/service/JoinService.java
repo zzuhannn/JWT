036f896a01503b2aa784280a738761481ac5e2c6
zzuhannn <lunaaeric41@naver.com>
2025-07-22 14:54:45 +0900

    feat: implement user registration logic in JoinService

---
+++ src/main/java/com/yumi/SpringJWT/service/JoinService.java
@@ -0,0 +1,35 @@
+package com.yumi.SpringJWT.service;
+
+import com.yumi.SpringJWT.dto.JoinDTO;
+import com.yumi.SpringJWT.entity.UserEntity;
+import com.yumi.SpringJWT.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JoinService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    public void joinProcess(JoinDTO joinDTO){
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+
+        if (isExist){
+            return;
+        }
+
+        UserEntity data = new UserEntity();
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+        userRepository.save(data);
+    }
+
+}
