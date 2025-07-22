664b10983a0502eb0928bd3ba605d518698d0ef9
zzuhannn <lunaaeric41@naver.com>
2025-07-22 15:03:06 +0900

    refactor: integrate JoinService into JoinController

--- src/main/java/com/yumi/SpringJWT/controller/JoinController.java
+++ src/main/java/com/yumi/SpringJWT/controller/JoinController.java
@@ -1,16 +1,26 @@
 package com.yumi.SpringJWT.controller;
 
 import com.yumi.SpringJWT.dto.JoinDTO;
+import com.yumi.SpringJWT.service.JoinService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
+@RequiredArgsConstructor
 public class JoinController {
+
+    private final JoinService joinService;
+
+
 
     @PostMapping("/join")
     public String joinProcess(JoinDTO joinDTO){
+
+        joinService.joinProcess(joinDTO);
+
         return "ok";
     }
 }
