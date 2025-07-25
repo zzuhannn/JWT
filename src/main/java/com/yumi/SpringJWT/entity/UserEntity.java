822f94fed2b5509ff948bf4b186d885b03476539
zzuhannn <lunaaeric41@naver.com>
2025-07-25 16:19:21 +0900

    feat: add Getter anotation in UserEntity

--- src/main/java/com/yumi/SpringJWT/entity/UserEntity.java
+++ src/main/java/com/yumi/SpringJWT/entity/UserEntity.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Setter
+@Getter
 public class UserEntity {
 
     @Id
