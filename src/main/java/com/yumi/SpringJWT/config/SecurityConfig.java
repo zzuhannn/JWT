03fec68194caf6534aaae49aa6506099c428ccc8
zzuhannn <lunaaeric41@naver.com>
2025-07-27 01:27:00 +0900

    feat: add cors setting in SecuritiyConfig

--- src/main/java/com/yumi/SpringJWT/config/SecurityConfig.java
+++ src/main/java/com/yumi/SpringJWT/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.yumi.SpringJWT.config;
 import com.yumi.SpringJWT.jwt.JWTFilter;
 import com.yumi.SpringJWT.jwt.JWTUtil;
 import com.yumi.SpringJWT.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -46,6 +51,25 @@ public class SecurityConfig {
 
 
         http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
+        http
                 .csrf((auth) -> auth.disable());
 
         http
@@ -72,4 +96,6 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
 }
